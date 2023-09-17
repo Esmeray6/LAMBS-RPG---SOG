b7db3da269e869e21c0ba96bffafda4d0b48e6fb
Assanali Mukhanov <22858781+Esmeray6@users.noreply.github.com>
2023-09-17 06:41:08 +0600

    Remove vanilla launchers

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -1,58 +1,18 @@
 class CfgPatches {
-  class lambs_rpg {
+  class LAMBS_RPG_SOG {
     units[] = {};
     weapons[] = {};
     requiredVersion = 1.82;
-    requiredAddons[] = {"A3_Weapons_F", "A3_Weapons_F_Exp", "A3_Weapons_F_Tank",
-                        "weapons_f_vietnam_c"};
+    requiredAddons[] = {"weapons_f_vietnam_c"};
     version = "1.52";
     versionStr = "1.52";
     author = "Esmeray";
-    authorUrl = "http://www.nopryl.no";
   };
 };
 
 class Mode_SemiAuto;
 class CfgWeapons {
-  class LauncherCore;
-  class Launcher;
-  class Launcher_Base_F;
-  class vn_Launcher_Base_F;
-  class launch_RPG7_F : Launcher_Base_F {
-    class Single : Mode_SemiAuto {
-      dispersion = 0.018;
-      aiDispersionCoefX = 1.8;
-      aiDispersionCoefY = 2.4;
-      aiRateOfFire = 5;
-      aiRateOfFireDispersion = 5;
-      aiRateOfFireDistance = 450;
-      maxRange = 450;
-      maxRangeProbab = 1.8;
-      midRange = 200;
-      midRangeProbab = 2.5;
-      minRange = 5;
-      minRangeProbab = 0.5;
-      recoil = "recoil_single_law";
-      sounds[] = {"StandardSound"};
-    };
-  };
-  class launch_RPG32_F : Launcher_Base_F {
-    class Single : Mode_SemiAuto {
-      aiDispersionCoefX = 1.7;
-      aiDispersionCoefY = 2.2;
-      aiRateOfFire = 7;
-      aiRateOfFireDispersion = 3;
-      aiRateOfFireDistance = 400;
-      maxRange = 600;
-      maxRangeProbab = 1.1;
-      midRange = 40;
-      midRangeProbab = 0.85;
-      minRange = 10;
-      minRangeProbab = 0.3;
-      recoil = "recoil_single_law";
-      sounds[] = {"StandardSound"};
-    };
-  };
+  // class vn_Launcher_Base_F;
   // class vn_m72 : vn_Launcher_Base_F {
   //   class Single : Mode_SemiAuto {
   //     dispersion = 0.018;
@@ -72,59 +32,37 @@ class CfgWeapons {
 };
 
 class CfgAmmo {
-  class RocketCore;
-  class RocketBase;
-  // class vn_rocket_ammo_base;
-  class R_PG7_F : RocketBase {
-    aiAmmoUsageFlags = "64 + 128 + 256 + 512";
-    airLock = 1;
-    allowAgainstInfantry = 1;
-    cost = 0;
-
-    // thrust = 470;
-    effectsMissile = "missile3";
-    whistleDist = 30;
-  };
-  class M_SPG9_HEAT : RocketBase {
-    aiAmmoUsageFlags = "64 + 128 + 512";
-    allowAgainstInfantry = 1;
-  };
-  class R_PG32V_F : RocketBase {
-    aiAmmoUsageFlags = "64 + 128 + 256 + 512";
-    airLock = 1;
-    allowAgainstInfantry = 1;
-    cost = 40;
-  };
-  class R_MRAAWS_HEAT_F : RocketBase {
-    aiAmmoUsageFlags = "64 + 128 + 512";
-    allowAgainstInfantry = 1;
-    cost = 40;
-  };
+  class vn_rocket_ammo_base;
   class vn_m72_rocket_ammo {
     allowAgainstInfantry = 1;
     cost = 70;
   };
-  class vn_rpg2_rocket_ammo {
-    allowAgainstInfantry = 1;
-    cost = 70;
-  };
 
-  // Inherits from vn_rpg2_rocket_ammo. Kinda pointless to have both.
+  // Inherits from vn_rpg2_rocket_ammo. Kinda pointless to have both, but I'll
+  // keep this here.
+
   // class vn_rpg2_fuze_rocket_ammo {
   //   allowAgainstInfantry = 1;
   //   cost = 70;
   // };
 
   // B41 only has one type of ammo.
-  class vn_rpg7_rocket_ammo {
+  class vn_rpg7_rocket_ammo : vn_rocket_ammo_base {
+    airLock = 1;
+    allowAgainstInfantry = 1;
+    cost = 70;
+  };
+  class vn_rpg2_rocket_ammo : vn_rpg7_rocket_ammo {
+    airLock = 1;
     allowAgainstInfantry = 1;
     cost = 70;
   };
   class vn_m20a1b1_wp_ammo {
     allowAgainstInfantry = 1;
     cost = 100;
-  };s
+  };
   class vn_m20a1b1_heat_ammo {
+    airLock = 1;
     allowAgainstInfantry = 1;
     cost = 100;
   };
--- addons/main/readme.md
+++ addons/main/readme.md
@@ -1,5 +1,5 @@
 # LAMBS RPG 
-This mod is a config level tweak for hand-hend launchers from the [SOG DLC](https://store.steampowered.com/app/1227700/Arma_3_Creator_DLC_SOG_Prairie_Fire/). The AI will act more aggressively when equipped with them. 
+This mod is a config level tweak for hand-held launchers from the [SOG DLC](https://store.steampowered.com/app/1227700/Arma_3_Creator_DLC_SOG_Prairie_Fire/). The AI will act more aggressively when equipped with them. 
 
 Configs of the following launchers are tweaked:
 - B40
